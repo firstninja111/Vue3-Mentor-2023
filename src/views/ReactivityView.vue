1b6fbd16bcf820b6e6a985359354a79f16779d00
firstninja111 <cloudrider.m92@gmail.com>
2023-12-01 11:14:53 +0200

    Reactivity Fundamentals

---
+++ src/views/ReactivityView.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+    import { reactive } from 'vue'
+    import { ref, computed } from 'vue'
+
+    interface Book {
+        title: string
+        year?: number
+    }
+    const book:Book = reactive({ title: 'Vue 3 Guide' })
+    
+    const count = ref(0)
+    const double = computed(() => count.value * 2)
+    const increase = () => {count.value++}
+
+</script>
+
+<template>
+    <div class="reactivity">
+        <p>{{ book.title }}</p>
+        <p @click="increase">{{ double }}</p>
+    </div>
+</template>
