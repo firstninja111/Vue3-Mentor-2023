659e0eee1d635823a0a3c09241478a99200388b6
firstninja111 <cloudrider.m92@gmail.com>
2023-12-14 11:40:49 +0200

    Add multiple slot test

--- src/views/Child/CustomInput.vue
+++ src/views/Child/CustomInput.vue
@@ -10,15 +10,19 @@
 </script>
 <template>
     <div>
+        <slot name="header"></slot>
+        <slot name="main"></slot>
         <input
             type="text"
             :value="firstName"
             @input="$emit('update:firstName', $event.target.value)"
-            />
-            <input
+        />
+        <input
             type="text"
             :value="lastName"
             @input="$emit('update:lastName', $event.target.value)"
-            />
+        />
+        <slot name="footer"></slot>
+        <slot name="dynamic-slot"></slot>
     </div>
 </template>
--- src/views/CustomEvent.vue
+++ src/views/CustomEvent.vue
@@ -11,7 +11,20 @@
         <CustomInput
             v-model:first-name="first"
             v-model:last-name="last"
-        />
+        >
+            <template #header>
+                <p>This is header</p>
+            </template>
+            <template #main>
+                <p>This is desc</p>
+            </template>
+            <template #footer>
+                <p>This is footer</p>
+            </template>
+            <template #dynamic-slot>
+                AAAA Customized Slot
+            </template>
+        </CustomInput>
         <p>First Name: {{ first }}</p>
         <p>Last Name: {{ last }}</p>
     </div>
